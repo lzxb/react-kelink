198d44571417338801841088e95072f8608a42a4
lzxb <1340641314@qq.com>
2016-06-10 16:43:53 +0800

    调整

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import route from './config/route';
 import store from './config/store';
-import './less/style.less'; //css文件
-import './iconfont/iconfont.css';
+import './css/resets.css'; //重置浏览器默认样式
+import './css/data-flex.min.css'; //css布局文件
+import './css/style.less'; //css文件
+import './iconfont/iconfont.css'; //字体图标文件
 
 store.subscribe(function () {
     //    console.log(store.getState());
