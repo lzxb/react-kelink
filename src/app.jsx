318557613985cc3fc54cafb9598767f66ffaceb4
lzxb <1340641314@qq.com>
2016-06-13 23:17:20 +0800

    修改css的错误

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import route from './config/route';
 import store from './config/store';
-import './css/resets.css'; //重置浏览器默认样式
+import './css/resets.less'; //重置浏览器默认样式
 import './css/data-flex.min.css'; //css布局文件
 import './css/style.less'; //css文件
 import './iconfont/iconfont.css'; //字体图标文件
